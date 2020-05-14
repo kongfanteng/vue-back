e13bfd894a74b3c4513b136710b0eade1697a9b8
kongfanteng <18210591009@163.com>
2020-05-14 17:30:30 +0800

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+let users = [{ id: 1, name: 'mk1' }, { id: 2, name: 'mk2' }, { id: 3, name: 'mk3' }];
+let server = http.createServer(function (req, res) {
+  // console.log(req.method, req.url);
+  if (req.url == '/api/users') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.end(JSON.stringify(users));
+  } else {
+    res.end('Not Fount!');
+  }
+})
+server.listen(3000, () => {
+  console.log('启动在3000端口');
+})
